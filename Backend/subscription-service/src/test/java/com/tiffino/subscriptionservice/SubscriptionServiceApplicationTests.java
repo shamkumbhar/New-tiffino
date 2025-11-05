57e550aeecb932fa44774013f620da11550467a4
shamkumbhar <shamkumbhar47@gmail.com>
2025-11-05 16:34:31 +0530

    Update SubscriptionServiceApplicationTests.java

--- Backend/subscription-service/src/test/java/com/tiffino/subscriptionservice/SubscriptionServiceApplicationTests.java
+++ Backend/subscription-service/src/test/java/com/tiffino/subscriptionservice/SubscriptionServiceApplicationTests.java
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ce24af0d7605259349d0fb709d9664c9e9279f38c754ec596b73a02e0f7587b7
-size 245
+package com.tiffino.subscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SubscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionServiceApplication.class, args);
+    }
+
+}
