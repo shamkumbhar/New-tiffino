a56f0610900bc02140f3b7ac58a352721978b38a
shamkumbhar <shamkumbhar47@gmail.com>
2025-11-05 13:33:01 +0530

    Update TiffinoApplication.java

--- Backend/src/main/java/com/tiffino/TiffinoApplication.java
+++ Backend/src/main/java/com/tiffino/TiffinoApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7d8edcd8b9eb91d8aac15c63b2d29fcba55d8c9b584166b792330f1c16058fe
-size 318
+package com.tiffino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiffinoApplication.class, args);
+    }
+
+}
